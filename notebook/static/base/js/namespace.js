28ec6baecc02658880ddccfc8eceec979f3237dd
amangarg96 <aman.garg1996@gmail.com>
2020-10-12 16:12:20 +0530

    use kernel managers session key instead of ZMQ connections session key as buffer key

--- notebook/static/base/js/namespace.js
+++ notebook/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "5.7.9.dev.4";
+    Jupyter.version = "5.7.9.dev5";
     Jupyter._target = '_blank';
     return Jupyter;
 });
